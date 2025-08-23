65533269cdb28563e9cff70e9d2d375fa921dd8c
MohammadAli Arjomand <arjomand.dev@gmail.com>
2025-08-23 15:36:41 +0330

    multiconfig option added

--- wg-gui.py
+++ wg-gui.py
@@ -2,11 +2,13 @@
 
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
+from tkinter import filedialog
 import subprocess
 
 win = tk.Tk()
 win.title("Wireguard Simple GUI")
-win.minsize(300, 150)
+win.minsize(300, 180)
 
 # check running as root
 def isRoot():
@@ -19,15 +21,15 @@ if not isRoot():
     messagebox.showerror("Permission denied", "You should be root to run this application.")
     exit()
 
-configName = "fast"
-
 def isConnected():
     output = subprocess.run(["wg", "show"], capture_output=True).stdout.decode()
     if len(output) > 0:     
-        return True
-    return False
+        firstLine = output.split("\n")[0]
+        activeConfig = firstLine.split(" ")[1]
+        return True, activeConfig
+    return False, ""
 
-connected = isConnected()
+connected, activeConfig = isConnected() # TODO: Complete here
 if not connected:
     btnText = "Connect"
 else:
@@ -35,6 +37,12 @@ else:
 
 def connection():
     global connected
+    configName = configsCombo.get()
+
+    if not checkConfigExists(configName):
+        messagebox.showerror("Config Error", "Chosen config does not exist")
+        return
+
     if connected:
         btn.config(text="Disconnecting...")
         win.update()
@@ -48,7 +56,43 @@ def connection():
         btn.config(text="Disconnect")
         connected = True
 
+def checkConfigExists(configName):
+    return len(subprocess.run(["ls", f"/etc/wireguard/{configName}.conf"], capture_output=True).stdout.decode()) > 0
+
+def listOfConfigs():
+    configs = subprocess.run(["ls", "/etc/wireguard/"], capture_output=True).stdout.decode().split("\n")
+    for i in range(len(configs)):
+        configs[i] = configs[i].split(".")[0]
+    configs.remove("")
+    return configs
+
+def newConfig():
+    configPath = filedialog.askopenfilename(
+        initialdir="/home",
+        title="Choose config file",
+        filetypes=(("Config files", "*.conf"), ("All files", "*"))
+    )
+    if configPath:
+        subprocess.run(["cp", configPath, "/etc/wireguard/"])
+        configsCombo.config(values=listOfConfigs())
+        configsCombo.set(configPath.split("/")[-1].split(".")[0])
+
+# GUI:
+
 tk.Label(win, text="\nWireGuard Simple GUI!\n").pack()
+
+configFrm = tk.Frame(win)
+configFrm.pack()
+
+configs = listOfConfigs()
+configsCombo = ttk.Combobox(configFrm, values=configs)
+configsCombo.set(configs[0])
+configsCombo.grid(row=0, column=0)
+
+newConfigBtn = tk.Button(configFrm, text="New Config", command=newConfig)
+newConfigBtn.grid(row=0, column=1)
+
+tk.Label(win, text="").pack()
 
 btn = tk.Button(win, text=btnText, command=connection)
 btn.pack()
